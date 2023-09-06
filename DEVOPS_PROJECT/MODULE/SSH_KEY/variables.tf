250837b6feeb017d8e76ef76cc1971209f4124bf
root <root@DESKTOP-AC3B9G6>
2023-09-06 11:50:08 +0530

    Terraform module

---
+++ DEVOPS_PROJECT/MODULE/SSH_KEY/variables.tf
@@ -0,0 +1,7 @@
+variable "M_keyname" {
+  type    = string
+}
+
+variable "M_public_key_path" {
+  type    = string
+}
---
+++ DEVOPS_PROJECT/variables.tf
@@ -0,0 +1,15 @@
+variable "L_keyname" {
+    type = string
+}
+
+variable "L_public_key_path" {
+    type = string
+}
+
+variable "L_ami_id" {
+    type = string
+}
+
+variable "L_instance_type" {
+    type = string
+}
